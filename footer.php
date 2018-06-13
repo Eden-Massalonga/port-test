1803185d406ecdf84a12159a009f601fa131dc3f
Eden-Massalonga <edenmassalonga@outlook.com>
2018-06-13 04:06:28 +0200

    Mudancas no projecto

---
+++ footer.php
@@ -0,0 +1,28 @@
+	
+	<footer class="fixed bg-dark mt-5 mb-0">
+		<div class="container row m-auto p-4">
+			<div class="col-sm-12 col-lg-6 mr-auto">
+				<h5 class="text-light"> Contactos:</h5>
+				<li class="list-group-item"><i class="fas fa-envelope"></i> Celular: 845769862</li>
+				<li class="list-group-item"><i class="fas fa-envelope"></i> E-mail: edenmassalonga@outlook.com</li>
+            </div>
+
+			<div class="col-sm-12 col-lg-6 ml-auto">
+				<h5 class="text-light"><i class="fas fa-address-book"></i> Siga-me:</h5>
+				<li class="list-group-item"><i class="fas fa-envelope"></i> Linked In</li>
+				<li class="list-group-item"><i class="fas fa-envelope"></i> Github</li>
+			</div>
+		</div>
+
+		<div class="container-fluid row bg-gradient-secondary justify-content-center m-auto p-2 ">
+			<p class="text-light mb-0">&copy; Direitos Reservados</p>
+		</div>
+	</footer>
+	
+	<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,82 @@
+<!--Header -->
+<?php include ("header.php"); ?>
+
+<!--Body-->
+    
+	<div class="container">
+		<br>
+		<div class="container-fluid">
+			
+			<div class="container">
+				<div class="bg-secondary text-light p-2">
+					<h3>Dados Pessoais</h3>
+				</div>
+				
+				<div class="row">
+					<div class="col-sm-3">
+						<img class="" src="img/profile-picture.jpg" alt="Card image cap">
+					</div>
+					
+					<div class="col-sm-9">
+					<div class="container">
+						<p class="card-text text-justify">Sou um Tecnico de Informatica formado na especialidade de Ciencias de Computacoes pelo Instituto Medio de Computacao e Gestao (IMPCG).</p>
+						<li class="list-group-item"><i class="fas fa-envelope"></i> blandinomucumbe@gmail.com</li>
+						<li class="list-group-item"><i class="fas fa-envelope"></i> edenmassalonga@outlook.com</li>
+						<li class="list-group-item"><i class="fas fa-envelope"></i> ediongungulo@gmail.com</li>
+						<li class="list-group-item"><i class="fas fa-envelope"></i> f2massimbe@gmail.com</li>
+					</div>
+					</div>
+				</div>								
+			</div>
+			
+			<div class="container">
+				<br>
+				<div class="">
+					<h3>Dados Pessoais</h3>
+				</div>
+				
+				<div class="row">
+					<div class="col-sm-3">
+						<img class="" src="img/profile-picture.jpg" alt="Card image cap">
+						<div class="mt-2">
+							<p class="card-text text-justify">Sou um Tecnico de Informatica formado na especialidade de Ciencias de Computacoes pelo Instituto Medio de Computacao e Gestao (IMPCG).</p>
+						</div>
+					</div>
+					
+					<div class="col-sm-9">
+						<li class="list-group-item"><i class="fas fa-envelope"></i> blandinomucumbe@gmail.com</li>
+						<li class="list-group-item"><i class="fas fa-envelope"></i> edenmassalonga@outlook.com</li>
+						<li class="list-group-item"><i class="fas fa-envelope"></i> ediongungulo@gmail.com</li>
+						<li class="list-group-item"><i class="fas fa-envelope"></i> f2massimbe@gmail.com</li>
+					</div>
+				</div>								
+			</div>
+			
+			<div class="container">
+				<br>
+				<div class="">
+					<h3>Dados Pessoais</h3>
+				</div>
+				
+				<div class="row">
+					<div class="col-sm-3">
+						<img class="rounded float-left" src="img/profile-picture.jpg" alt="Card image cap">
+						<div class="mt-2">
+							<p class="card-text text-justify">Sou um Tecnico de Informatica formado na especialidade de Ciencias de Computacoes pelo Instituto Medio de Computacao e Gestao (IMPCG).</p>
+						</div>
+					</div>
+					
+					<div class="col-sm-9">
+						<li class="list-group-item"><i class="fas fa-envelope"></i> blandinomucumbe@gmail.com</li>
+						<li class="list-group-item"><i class="fas fa-envelope"></i> edenmassalonga@outlook.com</li>
+						<li class="list-group-item"><i class="fas fa-envelope"></i> ediongungulo@gmail.com</li>
+						<li class="list-group-item"><i class="fas fa-envelope"></i> f2massimbe@gmail.com</li>
+					</div>
+				</div>								
+			</div>
+			
+		</div>	
+	</div>
+
+<!--footer-->
+<?php include ("footer.php"); ?>
